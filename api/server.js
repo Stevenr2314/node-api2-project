9d84fbc02ce09db4130841cd45e223f711f1a002
Steven Rosa <s.rosa21@icloud.com>
2022-01-11 19:11:28 -0500

    starting server structure

--- api/server.js
+++ api/server.js
@@ -1,2 +1,32 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+const server = express()
+
+server.use(express.json());
+
+server.get('/api/posts', (req, res) => {
+
+})
+
+server.get('/api/posts', (req, res) => {
+    
+})
+
+server.get('/api/posts', (req, res) => {
+    
+})
+
+server.post('/api/posts', (req, res) => {
+    
+})
+
+server.put('/api/posts', (req, res) => {
+    
+})
+
+server.delete('/api/posts', (req, res) => {
+    
+})
+
+module.exports = server
